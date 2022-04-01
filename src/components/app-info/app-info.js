c273719c762c92619b2044155d6c02ff3e5d9352
Marat <servrostov@gmail.com>
2022-04-01 17:41:11 +0300

    add Создал структуру проекта

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1>Учет сотрудников в компании ЭКСПЕРТ</h1>
+			<h2>Общее число сотрудников: </h2>
+			<h2>Приемию получат: </h2>
+		</div>
+	)
+}
+
+export default AppInfo;
---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import AppInfo from '../app-info/app-info';
+import './app.css';
+
+
+function App() {
+	return (
+		<div className='app'>
+			<AppInfo />
+		</div>
+	)
+}
+
+export default App;
