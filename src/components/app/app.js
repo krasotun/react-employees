fc4c26388f9fabf16d78e4b28bd8a9a752adfeae
krasotun <servrostov@gmail.com>
2022-04-01 21:53:47 +0300

    add Добавил структуру проекта

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,8 @@
 import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 
@@ -6,6 +10,12 @@ function App() {
 	return (
 		<div className='app'>
 			<AppInfo />
+			<div className="search-panel">
+				<SearchPanel />
+				<AppFilter />
+			</div>
+			<EmployeesList />
+			<EmployeesAddForm />
 		</div>
 	)
 }
