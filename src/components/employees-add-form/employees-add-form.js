86bcb9176a2170d847452d999d97a1e11b881f2e
krasotun <servrostov@gmail.com>
2022-06-12 23:16:17 +0300

    не передаются пропсы

---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import "./employees-add-form.css";
+
+class EmployeesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			salary: "",
+		};
+	}
+
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	onSubmit(event) {
+		event.preventDefault();
+		// console.log(this.props);
+	}
+	render() {
+		const { name, salary } = this.state;
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form className="add-form d-flex" onSubmit={this.onSubmit}>
+					<input
+						type="text"
+						className="form-control new-post-label"
+						placeholder="Как его зовут?"
+						name="name"
+						value={name}
+						onChange={this.onValueChange}
+					/>
+					<input
+						type="number"
+						className="form-control new-post-label"
+						placeholder="з/пл "
+						name="salary"
+						value={salary}
+						onChange={this.onValueChange}
+					/>
+					<button type="submit" className="btn btn-outline-light">
+						Добавить
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default EmployeesAddForm;
