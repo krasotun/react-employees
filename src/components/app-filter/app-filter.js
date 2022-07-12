a3bc089162e9436bec2401ef5a623fe68c4d1d9d
krasotun <servrostov@gmail.com>
2022-07-12 20:31:36 +0300

    add Добавил функционал

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -3,19 +3,19 @@ const foo = 123;
 console.log(foo);
 
 const AppFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-light" type="button">
-        Все сотрудники
-      </button>
-      <button className="btn btn-outline-light" type="button">
-        Сотрудники на повышение
-      </button>
-      <button className="btn btn-outline-light" type="button">
-        Зарплата больше $1000
-      </button>
-    </div>
-  );
+	return (
+		<div className="btn-group">
+			<button className="btn btn-light" type="button">
+				Все сотрудники
+			</button>
+			<button className="btn btn-outline-light" type="button">
+				Сотрудники на повышение
+			</button>
+			<button className="btn btn-outline-light" type="button">
+				Зарплата больше $1000
+			</button>
+		</div>
+	);
 };
 
 export default AppFilter;
