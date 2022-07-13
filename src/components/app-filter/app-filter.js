3dd11b455676af23a03ec78a210aeb0247749024
krasotun <servrostov@gmail.com>
2022-07-13 23:54:32 +0300

    add Создал табы

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,15 +1,51 @@
+import React from "react";
 import "./app-filter.css";
 const AppFilter = () => {
+
+	const [allButton, setAllButton] = React.useState(true);
+	const [riseButton, setRiseButton] = React.useState(false);
+	const [salaryButton, setSalaryButton] = React.useState(false);
+
+
+	const setButtonsNotActive = () => {
+		setAllButton(false);
+		setRiseButton(false);
+		setSalaryButton(false);
+	}
+	const resetFilters = () => {
+		setButtonsNotActive();
+		setAllButton(true);
+	}
+	const allButtonClick = () => {
+		setButtonsNotActive();
+		setAllButton(true);
+	}
+	const riseButtonClick = () => {
+		setButtonsNotActive();
+		setRiseButton(true);
+	}
+	const salaryButtonClick = () => {
+		setButtonsNotActive();
+		setSalaryButton(true);
+	}
+
 	return (
 		<div className="btn-group">
-			<button className="btn btn-light" type="button">
+			<button className={`btn ${allButton ? 'btn-light' : 'btn-outline-light'}`}
+				type="button" onClick={allButtonClick}>
 				Все сотрудники
 			</button>
-			<button className="btn btn-outline-light" type="button">
+			<button className={`btn ${riseButton ? 'btn-light' : 'btn-outline-light'}`}
+				type="button" onClick={riseButtonClick}>
 				Сотрудники на повышение
 			</button>
-			<button className="btn btn-outline-light" type="button">
+			<button className={`btn ${salaryButton ? 'btn-light' : 'btn-outline-light'}`}
+				type="button" onClick={salaryButtonClick}>
 				Зарплата больше $1000
+			</button>
+			<button className="btn btn-warning"
+				type="button" onClick={resetFilters}>
+				Сбросить фильтр
 			</button>
 		</div>
 	);
